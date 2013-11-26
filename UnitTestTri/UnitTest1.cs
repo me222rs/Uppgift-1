e6aa02da674f7ca7b65a5deee857f5ae5f0440d1
me222rs <me222rs@student.lnu.se>
2013-11-26 14:03:50 +0100

    Fler tester
    Lagt till 4 stycken tester, vet dock inte om de är nödvändiga.

--- UnitTestTri/UnitTest1.cs
+++ UnitTestTri/UnitTest1.cs
@@ -7,7 +7,7 @@ namespace UnitTestTri
     public class TriangleUnitTest
     {
 
-         //Equilateral och Scalene har blivit ihopblandade, det är anledningen till att de två testen failar
+         //Equilateral och Scalene har blivit ihopblandade, det är anledningen till att de två testen failar. Har rättat till felet så att det nu är korrekt
         [TestMethod]
         public void isIsoscelesTest()       //Likbent, 2 stycken lika sidor.
         {
@@ -57,7 +57,7 @@ namespace UnitTestTri
         }
 
         [TestMethod]
-        public void triangleConstructorTest3()
+        public void triangleConstructorTest3()          //Testar skapa en triangel med 3 punkter.
         {
             Point a = new Point(2, 2);
             Point b = new Point(4, 6);
@@ -68,5 +68,38 @@ namespace UnitTestTri
             Assert.IsFalse(Tri.isIsosceles());
         }
 
+
+
+        //Vet ej om dessa test är nödvändiga
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void triangleConstructor_Test_4_Less_Than_3_Points()       //Testar att skapa en triangel med fler än 3 punkter. Vet ej om detta är ett nödvändigt test
+        {
+            new Triangle(new[] { new Point(0, 0), new Point(1, 1) });
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]           //Testar att skapa en triangel med två stycken identiska punkter. Testar att undantag kastas.
+        public void triangleConstructorTest_5_Two_Identical_Points()
+        {
+            new Triangle(new[] { new Point(1, 1), new Point(1, 1) });
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]             //Testar att skapa en triangel där punkterna har värdet null.
+        public void triangleConstructorTest_6_Null_Points()
+        {
+            Point[] i = null;
+            Triangle target = new Triangle(i);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]                  //Testar att skapa en tom array.
+        public void triangleConstructorTest_7_Empty_Array()
+        {
+            new Triangle(new Point[] { });
+        }
     }
 }
