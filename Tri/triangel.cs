e97ed7275b1f858d2c9a10d7cc01d8768537073d
me222rs <me222rs@student.lnu.se>
2013-11-28 10:54:35 +0100

    Whitebox och blackbox klar
    Whitebox och blackbox är godkända med betyg 3+. Blackboxtesterna är
    tillagda i ett pdf dokument.

--- Tri/triangel.cs
+++ Tri/triangel.cs
@@ -18,23 +18,23 @@ public class Triangle {
     sides = new double[] { a, b, c };
   } 
 
-  public Triangle(double[] s) {
+  public Triangle(double[] s) {     
     sides = new double[s.Length];
     for(int i=0;i<s.Length;i++)
       sides[i]=s[i];
   } 
 
-  public Triangle(Point a, Point b, Point c) {      //Koordinatvärden
+  public Triangle(Point a, Point b, Point c) {      //Koordinatvärden       
     sides = new double[3];
     sides[0] = Math.Sqrt(Math.Pow((double)(b.x - a.x), 2.0) + Math.Pow((double)(b.y - a.y), 2.0));
     sides[1] = Math.Sqrt(Math.Pow((double)(b.x - c.x), 2.0) + Math.Pow((double)(b.x - a.x), 2.0));
     sides[2] = Math.Sqrt(Math.Pow((double)(c.x - a.x), 2.0) + Math.Pow((double)(c.x - a.x), 2.0));
   }
 
-  public Triangle(Point[] s) {
+  public Triangle(Point[] s) {              
     sides = new double[s.Length];
     sides[0] = Math.Sqrt(Math.Pow((double)(s[1].x - s[0].x), 2.0) + Math.Pow((double)(s[1].y - s[0].y), 2.0));
-    sides[1] = Math.Sqrt(Math.Pow((double)(s[1].x - s[2].x), 2.0) + Math.Pow((double)(s[1].x - s[0/*Ändrat här*/].x), 2.0));  //Ändrat [2] till [0] för att det ska fungera
+    sides[1] = Math.Sqrt(Math.Pow((double)(s[1].x - s[2].x), 2.0) + Math.Pow((double)(s[1].x - s[0/*Ändrat här*/].x), 2.0));  //Ändrat [2] till [0] för att det ska fungera korrekt
     sides[2] = Math.Sqrt(Math.Pow((double)(s[2].x - s[0].x), 2.0) + Math.Pow((double)(s[2].x - s[0].x), 2.0));
   }
 
--- UnitTestTri/UnitTest1.cs
+++ UnitTestTri/UnitTest1.cs
@@ -11,8 +11,8 @@ namespace UnitTestTri
         [TestMethod]
         public void isIsoscelesTest()       //Likbent, 2 stycken lika sidor.
         {
-            Triangle Tri = new Triangle(1, 2, 1);
-            Assert.IsTrue(Tri.isIsosceles());
+            Triangle Tri = new Triangle(4, 2, 4);       //Skapar likbent triangel med sidorna 1 2 1
+            Assert.IsTrue(Tri.isIsosceles());           //Om den är likbent blir det true, annars false
             Assert.IsFalse(Tri.isScalene());
             Assert.IsFalse(Tri.isEquilateral());
         }
@@ -20,8 +20,8 @@ namespace UnitTestTri
         [TestMethod]
         public void isEquilateralTest()     //Liksidig, alla sidor är lika.
         {
-            Triangle Tri = new Triangle(1.0, 1.0, 1.0);
-            Assert.IsTrue(Tri.isEquilateral());
+            Triangle Tri = new Triangle(1.0, 1.0, 1.0);     //Skapar liksidig triangel med sidorna 1.0 1.0 1.0
+            Assert.IsTrue(Tri.isEquilateral());             //Om den är liksidig blir det true, annars false
             Assert.IsFalse(Tri.isScalene());
             Assert.IsFalse(Tri.isIsosceles());
         }
@@ -29,8 +29,8 @@ namespace UnitTestTri
         [TestMethod]
         public void isScaleneTest()             //Oliksidig, alla sidor är olika.
         {
-            Triangle Tri = new Triangle(3, 4, 5);
-            Assert.IsTrue(Tri.isScalene());
+            Triangle Tri = new Triangle(3, 4, 5);       //Skapar oliksidig triangel med sidorna 3 4 5
+            Assert.IsTrue(Tri.isScalene());             //Om den är oliksidig blir det true, annars false
             Assert.IsFalse(Tri.isEquilateral());
             Assert.IsFalse(Tri.isIsosceles());
         }
@@ -38,11 +38,11 @@ namespace UnitTestTri
         [TestMethod]
         public void triangleConstructorTest()       //Det som ligger i parametrarna i point är koordinater
         {
-            Point a = new Point(2, 2);
+            Point a = new Point(2, 2);              //De kordinatererna som finns bildar en giltig oliksidig triangel
             Point b = new Point(4, 6);
             Point c = new Point(10, 12);
-            Triangle Tri = new Triangle(a, b, c);
-            Assert.IsTrue(Tri.isScalene());
+            Triangle Tri = new Triangle(a, b, c);       //Skapar en oliksidig triangel med de tre koordinaterna
+            Assert.IsTrue(Tri.isScalene());             //Om den är oliksidig blir det true, annars false
             Assert.IsFalse(Tri.isEquilateral());
             Assert.IsFalse(Tri.isIsosceles());
         }
@@ -50,7 +50,7 @@ namespace UnitTestTri
         [TestMethod]
         public void triangleConstructorTest2()      //Testar arraykonstruktorn genom att skicka in 3 värden i arrayen
         {
-            Triangle Tri = new Triangle(new double[] {3, 4, 5});    
+            Triangle Tri = new Triangle(new double[] {3, 4, 5});    //Skapar en triangel som innehåller tre arrayelement med värdena 3 4 5
             Assert.IsTrue(Tri.isScalene());
             Assert.IsFalse(Tri.isEquilateral());
             Assert.IsFalse(Tri.isIsosceles());
@@ -59,7 +59,7 @@ namespace UnitTestTri
         [TestMethod]
         public void triangleConstructorTest3()          //Testar skapa en triangel med 3 punkter.
         {
-            Point a = new Point(2, 2);
+            Point a = new Point(2, 2);                  //De kordinatererna som finns bildar en giltig oliksidig triangel
             Point b = new Point(4, 6);
             Point c = new Point(10, 12);
             Triangle Tri = new Triangle(new Point[] { a, b, c });
@@ -70,36 +70,53 @@ namespace UnitTestTri
 
 
 
-        //Vet ej om dessa test är nödvändiga
-        [TestMethod]
-        [ExpectedException(typeof(IndexOutOfRangeException))]
-        public void triangleConstructor_Test_4_Less_Than_3_Points()       //Testar att skapa en triangel med fler än 3 punkter. Vet ej om detta är ett nödvändigt test
-        {
-            new Triangle(new[] { new Point(0, 0), new Point(1, 1) });
-        }
 
 
-        [TestMethod]
-        [ExpectedException(typeof(IndexOutOfRangeException))]           //Testar att skapa en triangel med två stycken identiska punkter. Testar att undantag kastas.
-        public void triangleConstructorTest_5_Two_Identical_Points()
-        {
-            new Triangle(new[] { new Point(1, 1), new Point(1, 1) });
-        }
 
 
-        [TestMethod]
-        [ExpectedException(typeof(NullReferenceException))]             //Testar att skapa en triangel där punkterna har värdet null.
-        public void triangleConstructorTest_6_Null_Points()
-        {
-            Point[] i = null;
-            Triangle target = new Triangle(i);
-        }
 
-        [TestMethod]
-        [ExpectedException(typeof(IndexOutOfRangeException))]                  //Testar att skapa en tom array.
-        public void triangleConstructorTest_7_Empty_Array()
-        {
-            new Triangle(new Point[] { });
-        }
+
+
+
+
+
+
+
+
+
+
+
+
+        ////Vet ej om dessa test är nödvändiga
+        //[TestMethod]
+        //[ExpectedException(typeof(IndexOutOfRangeException))]
+        //public void triangleConstructor_Test_4_Less_Than_3_Points()       //Testar att skapa en triangel med fler än 3 punkter. Vet ej om detta är ett nödvändigt test
+        //{
+        //    new Triangle(new[] { new Point(0, 0), new Point(1, 1) });
+        //}
+
+
+        //[TestMethod]
+        //[ExpectedException(typeof(IndexOutOfRangeException))]           //Testar att skapa en triangel med två stycken identiska punkter. Testar att undantag kastas.
+        //public void triangleConstructorTest_5_Two_Identical_Points()
+        //{
+        //    new Triangle(new[] { new Point(1, 1), new Point(1, 1) });
+        //}
+
+
+        //[TestMethod]
+        //[ExpectedException(typeof(NullReferenceException))]             //Testar att skapa en triangel där punkterna har värdet null.
+        //public void triangleConstructorTest_6_Null_Points()
+        //{
+        //    Point[] i = null;
+        //    Triangle target = new Triangle(i);
+        //}
+
+        //[TestMethod]
+        //[ExpectedException(typeof(IndexOutOfRangeException))]                  //Testar att skapa en tom array.
+        //public void triangleConstructorTest_7_Empty_Array()
+        //{
+        //    new Triangle(new Point[] { });
+        //}
     }
 }
